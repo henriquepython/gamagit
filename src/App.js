916f3761572ed0cf29c1197fdd9103b09dc4ec48
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-08-11 10:36:05 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App(props) {
+  const [ usuario, setUsuario] = useState('Guilherme')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário"></input>
+      <button type="button">Pesquisar</button>
+    </>
   );
 }
 
