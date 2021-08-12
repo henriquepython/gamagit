d0f2f58d2ae93ff10ec4f2d836b4bf5b8ffdcfa4
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-08-12 12:14:56 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import Routes from './routes';
+import './style.css';
 
-function App(props) {
-  const [ usuario, setUsuario] = useState('');
+function App() {
   
-  function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
-  }
   return (
-    <>      
-      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
-    </>
+    <Routes />
   );
 }
 
--- src/Repositories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Repositories() {
-    return (
-        <h1>Repositories</h1>
-    )
-}
